6286bbbb591dd1c11cc0f322dc3ddea398025495
VariantBSP <91957967+VariantBSP@users.noreply.github.com>
2023-02-16 04:19:22 -0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const posts = [
+    {
+        id: 1,
+        title: "Ex-president Seen At Bar",
+    },
+    {
+        id: 2,
+        title: "Coming Soon...",
+    },
+    {
+        id: 3,
+        title: "Inspira to launch in 2024",
+    },
+    {
+        id: 4,
+        title: "Choose Violence Today No Peace!!!",
+    }
+]
+
+posts.map(function(post){
+    return (
+        console.log(post.title)
+    )
+})
+
